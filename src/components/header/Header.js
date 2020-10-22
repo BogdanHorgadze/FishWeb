af52b724774ad721dd4ffb952c57c1110e707e9c
unknown <beerbaron322@gmail.com>
2020-10-22 17:46:26 +0300

    add left-side div

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './header.module.scss'
 import { NavBar } from '../NavBar/NavBar'
 import { Logo } from '../Logo/Logo'
+import { Search } from '../Search/Search'
 
 export const Header = () => {
     console.log(styles)
@@ -10,10 +11,10 @@ export const Header = () => {
             <div className={`${styles['header-wrap']} content`}>
                 <Logo />
                 <NavBar />
+                <div className={styles['left-side']}>
+                <Search />
                 <div className={styles['cart-img']}>
                 </div>
-                <div className={styles.search}>
-                    
                 </div>
             </div>
         </header>
